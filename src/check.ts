f725555796a75e8827daf06e59e71ff4f5f02d69
Predrag Stojadinovic <predrags@nvidia.com>
2023-02-03 11:15:39 +0100

    chore: adding glyphs

--- src/check.ts
+++ src/check.ts
@@ -42,13 +42,13 @@ export default {
 		if (!_.isEmpty(srcFiles)) {
 			console.log(clc.red('\nSource files missing the ' + clc.italic("'use strict';") + ' statement:'));
 			console.log(clc.red(getTableFromFileObjects(convertFilesToObjects(srcFiles)).toString()));
-		} else console.log(clc.green('\nNo source files are missing the ' + clc.italic("'use strict';") + ' statement.'));
+		} else console.log(clc.green('\n✅ No source files are missing the ' + clc.italic("'use strict';") + ' statement.'));
 
 		testFiles = _.filter(testFiles, missingStrictStatement);
 		if (!_.isEmpty(testFiles)) {
 			console.log(clc.red('\nTest files missing the ' + clc.italic("'use strict';") + ' statement:'));
 			console.log(clc.red(getTableFromFileObjects(convertFilesToObjects(testFiles)).toString()));
-		} else console.log(clc.green('\nNo test files are missing the ' + clc.italic("'use strict';") + ' statement.'));
+		} else console.log(clc.green('\n✅ No test files are missing the ' + clc.italic("'use strict';") + ' statement.'));
 
 		if (add) {
 			if (_.isEmpty(srcFiles) && _.isEmpty(testFiles)) {
@@ -73,6 +73,8 @@ export default {
 				if (!_.isEmpty(testFiles)) message += 'There are test files missing the ' + clc.italic("'use strict';") + ' statement!\n';
 
 				bail(clc.red(message));
+			} else {
+				console.log(clc.green('\n✅ Everything is awesome!\n'));
 			}
 		}
 	}
