c1fd3d0af8243608f3339a2f2d2e8f1a832dc3e9
Predrag Stojadinovic <predrags@nvidia.com>
2023-02-03 11:22:59 +0100

    chore: bolding the "error"

--- src/check.ts
+++ src/check.ts
@@ -42,13 +42,13 @@ export default {
 		if (!_.isEmpty(srcFiles)) {
 			console.log(clc.red('\nSource files missing the ' + clc.italic("'use strict';") + ' statement:'));
 			console.log(clc.red(getTableFromFileObjects(convertFilesToObjects(srcFiles)).toString()));
-		} else console.log(clc.green('\n✅ No source files are missing the ' + clc.italic("'use strict';") + ' statement.'));
+		} else console.log(clc.green('\n✅  No source files are missing the ' + clc.italic("'use strict';") + ' statement.'));
 
 		testFiles = _.filter(testFiles, missingStrictStatement);
 		if (!_.isEmpty(testFiles)) {
 			console.log(clc.red('\nTest files missing the ' + clc.italic("'use strict';") + ' statement:'));
 			console.log(clc.red(getTableFromFileObjects(convertFilesToObjects(testFiles)).toString()));
-		} else console.log(clc.green('\n✅ No test files are missing the ' + clc.italic("'use strict';") + ' statement.'));
+		} else console.log(clc.green('\n✅  No test files are missing the ' + clc.italic("'use strict';") + ' statement.'));
 
 		if (add) {
 			if (_.isEmpty(srcFiles) && _.isEmpty(testFiles)) {
@@ -69,12 +69,12 @@ export default {
 		} else {
 			if (!_.isEmpty(srcFiles) || !_.isEmpty(testFiles)) {
 				let message = '\n';
-				if (!_.isEmpty(srcFiles)) message += 'There are source files missing the ' + clc.italic("'use strict';") + ' statement!\n';
-				if (!_.isEmpty(testFiles)) message += 'There are test files missing the ' + clc.italic("'use strict';") + ' statement!\n';
+				if (!_.isEmpty(srcFiles)) message += clc.bold('ERROR:') + ' There are source files missing the ' + clc.italic("'use strict';") + ' statement!\n';
+				if (!_.isEmpty(testFiles)) message += clc.bold('ERROR:') + ' There are test files missing the ' + clc.italic("'use strict';") + ' statement!\n';
 
 				bail(clc.red(message));
 			} else {
-				console.log(clc.green('\n✅ Everything is awesome!\n'));
+				console.log(clc.green('\n✅  Everything is awesome!\n'));
 			}
 		}
 	}
